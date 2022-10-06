3abce174caf9453b078b0fc27b4a50e444058fb6
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-10-06 09:41:57 -0400

    MergingP

---
+++ CodeFile1.cs
@@ -0,0 +1,17 @@
+﻿internal class Student
+{
+    static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>();
+
+        students.Add(new Student("Jimmy", 13));
+        students.Add(new Student("Hannah Banana", 21));
+        students.Add(new Student("Justin", 30));
+        students.Add(new Student("Sarah", 53));
+        students.Add(new Student("Hannibal", 15));
+        students.Add(new Student("Phillip", 16));
+        students.Add(new Student("Maria", 63));
+        students.Add(new Student("Abe", 33));
+        students.Add(new Student("Curtis", 10));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace L8Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 2330, 112233, 10, 949, 3764, 2942 };
+
+            string[] students =
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Jimmy", 13));
+            students.Add(new Student("Hannah Banana", 21));
+            students.Add(new Student("Justin", 30));
+            students.Add(new Student("Sarah", 53));
+            students.Add(new Student("Hannibal", 15));
+            students.Add(new Student("Phillip", 16));
+            students.Add(new Student("Maria", 63));
+            students.Add(new Student("Abe", 33));
+            students.Add(new Student("Curtis", 10));
+
+            Console.WriteLine("I suck");
+        }
+    }
+}
